6229d00f2072eb2c36a5c7f4788e5e487413fe34
unknown <sharjeelabbas014@gmail.com>
2021-10-03 12:29:52 +0500

    Added more routes

--- .docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/638edb1791d01184dab898278e77a517.js
+++
@@ -1,8 +0,0 @@
-import { Code } from 'gatsby-theme-docz/src/components/Code';
-import { Playground } from "docz";
-import * as React from 'react';
-export default {
-  Code,
-  Playground,
-  React
-};
